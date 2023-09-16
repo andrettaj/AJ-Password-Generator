16a944b2524bca54865a617194f64d9495ab64dc
andrettaj <andrettaj@aol.com>
2023-09-16 11:48:39 -0700

    Validating acceptable characters

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 // Assignment code here
 
-function generatePassword () {
-var passwordlength = parseInt(prompt("Enter Password Length"))
-console.log (passwordlength)
-if (passwordlength <8 || passwordlength>128 || isNaN(passwordlength)){
-  return "Enter valid password length. "
-} 
+function generatePassword() {
+  var passwordlength = parseInt(prompt("Enter Password Length"))
+  console.log(passwordlength)
+  if (passwordlength < 8 || passwordlength > 128 || isNaN(passwordlength)) {
+    return "Enter valid password length. "
+  }
+  var uppercase = confirm("Do you want uppercase letters?")
+  var lowercase = confirm("Do you want lowercase letters?")
+  var number = confirm("Do you want a number?")
+  var specialcharacter = confirm("Do you want special Charactor?")
+  console.log(passwordlength)
+  var acceptableChars = []
+  if (uppercase) {
+    acceptableChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+
+  if (lowercase) {
+    acceptableChars += "abcdefghijklmnopqrstuvwxyz"
+  }
+  if (number) {
+    acceptableChars += "0123456789"
+  }
+  if (specialcharacter) {
+    acceptableChars += "~!@#$%^&*()+_-="
+  }
+  console.log (acceptableChars)
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
